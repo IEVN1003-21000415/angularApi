998ebbecda35ccf45ee676f2c9cfd5e93bfe0a9c
Dulce Belman <jimedulbelman@gmail.com>
2024-10-25 19:20:01 -0600

    angularApi 25-10-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TempComponent } from "./tem/temp/temp.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TempComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularApi';
+  title = 'angularAPI';
 }
---
+++ src/app/tem/temh/temh.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+ 
+@Component({
+  selector: 'app-temh',
+  standalone: true,
+  imports: [],
+  templateUrl: './temh.component.html',
+  styles: ``
+})
+export class TemhComponent {
+ 
+  @Input() mensaje:string=''
+ 
+}
